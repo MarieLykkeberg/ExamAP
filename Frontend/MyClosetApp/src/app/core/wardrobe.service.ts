8ff5f25b9c1d89051e526d20dfdfe9db04391cc7
Marie Lykkeberg <mariealykkeberg@gmail.com>
2025-04-22 22:38:08 +0200

    creation of full wardrobe

--- Frontend/MyClosetApp/src/app/core/wardrobe.service.ts
+++ Frontend/MyClosetApp/src/app/core/wardrobe.service.ts
@@ -2,22 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class WardrobeService {
-  private baseUrl = 'http://localhost:5196/api'; 
-
   constructor(private http: HttpClient) {}
 
   getItems(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/item`);
-  }
-  }
-/* 
-  getBottoms(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/bottom`);
+    return this.http.get<any[]>('http://localhost:5196/api/item');
   }
 
-  getFootwear(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/footwear`);
+  getCategories(): Observable<{ categoryId: number, categoryName: string }[]> {
+    return this.http.get<{ categoryId: number, categoryName: string }[]>('http://localhost:5196/api/category');
   }
-} */
+}
