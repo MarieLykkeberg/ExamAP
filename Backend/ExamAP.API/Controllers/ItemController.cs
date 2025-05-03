ccbb25f09f15c7fcd4e104c065ba10dce7c42170
isamilla <isamilla@harrit.com>
2025-05-03 11:43:34 +0200

    Middleware

--- Backend/ExamAP.API/Controllers/ItemController.cs
+++ Backend/ExamAP.API/Controllers/ItemController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using ExamAP.Model.Repositories;
 using ExamAP.Model.Entities;
 
 namespace ExamAP.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ItemController : ControllerBase
